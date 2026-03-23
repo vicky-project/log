653d9f19e09f63e55f7fff40c3d5e981185eec1b
vicky <pratamavr@gmail.com>
2026-03-23 12:44:44 +0800

    updates

--- app/Http/Controllers/AppLogController.php
+++ app/Http/Controllers/AppLogController.php
@@ -13,40 +13,4 @@ class AppLogController extends Controller
   public function index() {
     return view('log::logs.app');
   }
-
-  /**
-  * Show the form for creating a new resource.
-  */
-  public function create() {
-    return view('log::create');
-  }
-
-  /**
-  * Store a newly created resource in storage.
-  */
-  public function store(Request $request) {}
-
-  /**
-  * Show the specified resource.
-  */
-  public function show($id) {
-    return view('log::show');
-  }
-
-  /**
-  * Show the form for editing the specified resource.
-  */
-  public function edit($id) {
-    return view('log::edit');
-  }
-
-  /**
-  * Update the specified resource in storage.
-  */
-  public function update(Request $request, $id) {}
-
-  /**
-  * Remove the specified resource from storage.
-  */
-  public function destroy($id) {}
 }
