63de2ba91b246f49ee3102a16735742edb64807b
vicky <pratamavr@gmail.com>
2026-03-23 10:13:25 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,4 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Log\Http\Controllers\LogController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('logs', LogController::class)->names('log');
-});
+use Modules\Log\Http\Controllers\AppLogController;
