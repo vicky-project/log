0ea5809797a5b6557bd350f01d5e7f64bb2d24dc
vicky <pratamavr@gmail.com>
2026-04-01 19:43:54 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Modules\Log\Http\Controllers\AppLogController;
 use Modules\Log\Http\Controllers\ActivityLogController;
 use Modules\Log\Http\Controllers\AuthLogController;
 use Modules\Log\Http\Controllers\ScheduleMonitorController;
+use Rap2hpoutre\LaravelLogViewer\LogViewerController;
 
 Route::prefix('admin')
 ->name('admin.')
@@ -13,7 +14,7 @@ Route::prefix('admin')
   Route::prefix('logs')
   ->name('logs.')
   ->group(function() {
-    Route::get('app', [AppLogController::class, 'index'])->name('app');
+    Route::get('app', [LogViewerController::class, 'index'])->name('app');
     Route::get('auth', [AuthLogController::class, 'index'])->name('auth');
     Route::get('auth/{auth_log}', [AuthLogController::class, 'show'])->name('auth.show');
     Route::get("activity", [ActivityLogController::class, "index"])->name("activity");
