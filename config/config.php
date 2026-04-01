60d16dc0ab5a9ba6d3e73aff683627deae3ced5b
vicky <pratamavr@gmail.com>
2026-04-01 11:15:15 +0800

    updates

--- config/config.php
+++ config/config.php
@@ -8,5 +8,9 @@ return [
   ],
   "pruning" => [
     "retention_days" => env("LOG_RETENTION_DAYS", 30)
+  ],
+  "notifications" => [
+    "user_id" => env("LOG_NOTIFICATION_IDS", 1), // User Id in table users. Comma separated
+    "channels" => env("LOG_NOTIFICATION_CHANNELS", "database")
   ]
 ];
